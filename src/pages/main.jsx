8a53366a6486407875b09cae96d9c689ba742d20
yyz <surpriseye@qq.com>
2024-10-21 22:43:18 +0800

    增加craco

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -7,6 +7,7 @@ import {
   VideoCameraOutlined,
 } from '@ant-design/icons'
 import { Button, Layout, Menu, theme } from 'antd'
+import CommonAside from '../components/commonAside'
 
 const { Header, Sider, Content } = Layout;
 
@@ -17,34 +18,7 @@ const Main = () => {
   } = theme.useToken();
   return (
     <Layout className='main-container'>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <h3 className='app-name'>Timelist</h3>
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          items={[
-            {
-              key: '1',
-              icon: <UserOutlined />,
-              label: 'nav 1',
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
-            },
-            {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3',
-            },
-          ]}
-					style={{
-						height: '100%'
-					}}
-        />
-      </Sider>
+      <CommonAside/>
       <Layout>
         <Header
           style={{
@@ -54,8 +28,6 @@ const Main = () => {
         >
           <Button
             type="text"
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            onClick={() => setCollapsed(!collapsed)}
             style={{
               fontSize: '16px',
               width: 64,
